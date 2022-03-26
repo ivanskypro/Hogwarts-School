f63e0c423a1a6be0a8c9f01c31cf731437dee68b
ivanskypro <kalmych@gmail.com>
2022-03-26 14:40:05 +0300

    try1

---
+++ src/main/java/ru/hogwarts/hogwartsschool/scripts42.sql
@@ -0,0 +1,12 @@
+ALTER TABLE student
+ADD CONSTRAINT age_checking CHECK ( age>=16 );
+
+ALTER TABLE student
+ADD CONSTRAINT name_unique_check UNIQUE (name);
+
+ALTER TABLE student
+ALTER COLUMN name SET NOT NULL;
+
+ALTER TABLE student
+ALTER COLUMN age SET DEFAULT 20;
+
